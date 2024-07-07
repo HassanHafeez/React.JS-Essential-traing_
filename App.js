4bf831b1ebe602e4d366aca71e36e1b5666cae3a
Hassan Hafeez <108194691+HassanHafeez@users.noreply.github.com>
2024-07-08 03:02:03 +0500

    App.js
    App js file
    need all dependencies to use react js

---
+++ App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import ManagingState from './ManagingState';
+import ManagingState2 from './managingState2'
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello G, Saray ya nu!</p>
+      <hr/>
+      <ManagingState/>
+      <hr/>
+      <ManagingState2/>
+    </div>
+  );
+}
+
+export default App;
